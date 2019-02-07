49a8dbecbeb52b917c6c3da859720179bdccee51
adr2403 <adarshr2403@gmail.com>
2019-02-07 15:39:01 +0530

    trignometric functions

--- list.py
+++ list.py
@@ -1,6 +1,3 @@
 subjects=["physics","chemistry","maths",123]
-n=len(subjects)
-i=0
-while i<n:
- print(subjects[i])
- i=i+1
+del subjects[0]
+print(subjects)
---
+++ tuple.py
@@ -0,0 +1,5 @@
+trignometry=("sin","cos","tan","cosec","sec","cot")
+i=0
+while i<(len(trignometry)):
+ print(trignometry[i])
+ i=i+1
