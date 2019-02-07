222b92734f9f600d61765ca1cdac73cb0db304a0
adr2403 <adarshr2403@gmail.com>
2019-02-07 15:51:22 +0530

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+student={"name":"Adarsh","age":"18","mob":"86064*****"}
+ch=int(input("enter choice"))
+if ch==1:
+    print(student["name"])
+elif ch==2:
+    print(student["age"]) 
+else:
+    print(student["mob"])       
